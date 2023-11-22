529057afd8e15ba80dba79580cc4ed3fbe73bee0
Igor <igglike@gmail.com>
2023-11-22 17:46:17 +0600

    Добавил redux, сделал редьюсер для счётчика

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,35 @@
-import ReactDOM from 'react-dom/client'
-import {BrowserRouter} from 'react-router-dom'
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { legacy_createStore as createStore } from "redux";
 
-import './index.css'
-import App from './App'
+import "./index.css";
+import App from "./App";
+
+const initialState = {
+  counter: 100,
+};
+
+// {type: "INCREASE"}
+// {type: "DECREASE"}
+const counterReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "INCREASE":
+      return { ...state, counter: state.counter + 1 };
+    case "DECREASE":
+      return { ...state, counter: state.counter - 1 };
+    default:
+      return state;
+  }
+};
 
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const store = createStore(counterReducer);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-)
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>
+);
